22946ebe211a262eaa414d0006633d3a65b9ff4f
maxiaoqu <maxiaoqu@163.com>
2020-04-28 19:36:34 +0800

    报错未找到原因，后续再进行排查！

--- src/day02-code/Demo02AnuUi.jsx
+++ src/day02-code/Demo02AnuUi.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { Button } from 'antd';
 
-export default class demo02AnuUi extends React.Component {
+export default class Demo02AnuUi extends React.Component {
     render() {
         return (
             <div>
